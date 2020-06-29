ac2aca40852ba71f249e12fb157ffa59967a6bf5
abhinav anshul <abhinavanshul4u@gmail.com>
2020-06-29 10:48:47 +0530

    Login Page skeleton

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 /* eslint-disable prettier/prettier */
 
 import React from 'react'
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
-import { Main } from '@aragon/ui'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+// import { Main } from '@aragon/ui'
 
-import theme from './theme-court'
+// import theme from './theme-court'
 
-import MainView from './components/MainView'
-import ErrorPage from './ErrorPage'
+// import MainView from './components/MainView'
+// import ErrorPage from './ErrorPage'
 
-import Dashboard from './components/Dashboard/Dashboard'
-/* eslint-disable prettier/prettier */
+// import Dashboard from './components/Dashboard/Dashboard'
+import LoginPage from './components/Login/LoginPage'
+// /* eslint-disable prettier/prettier */
 
-import Tasks from './components/Tasks/Tasks'
-import Sample from './components/Sample/Sample'
-import Disputes from './components/Disputes/Disputes'
+// import Tasks from './components/Tasks/Tasks'
+// import Sample from './components/Sample/Sample'
+// import Disputes from './components/Disputes/Disputes'
 import { WalletProvider } from './providers/Wallet'
 import { ActivityProvider } from './components/Activity/ActivityProvider'
 
@@ -23,19 +24,9 @@ function App() {
     <WalletProvider>
       <BrowserRouter>
         <ActivityProvider>
-        <Main layout={false} theme={theme}>
-          <MainView>
-            <Redirect from='/' to='/dashboard' />
-            <Switch>
-              <Route exact path='/profile' component={Dashboard} />
-              <Route exact path='/leaderboard' component={Tasks} />
-              <Route exact path='/dashboard' component={Disputes} />
-              <Route exact path='/sample' component={Sample} />
-
-              <Route component={ErrorPage} />
-            </Switch>
-          </MainView>
-        </Main>
+          <Switch>
+            <Route exact path='/login' component={LoginPage} />
+          </Switch>
         </ActivityProvider>
       </BrowserRouter>
     </WalletProvider>
@@ -43,3 +34,36 @@ function App() {
 }
 
 export default App
+
+/*
+//// REPLACED ///////////////////
+
+
+<WalletProvider>
+      <BrowserRouter>
+        <ActivityProvider>
+         
+          <Main layout={false} theme={theme}>
+            <MainView>
+               <Redirect from='/' to='/dashboard' /> 
+              <Switch>
+                <Route exact path='/profile' component={Dashboard} />
+                <Route exact path='/leaderboard' component={Tasks} />
+                <Route exact path='/dashboard' component={Disputes} />
+                <Route exact path='/sample' component={Sample} />
+                <Route component={ErrorPage} />
+              </Switch>
+            </MainView>
+          </Main>
+        </ActivityProvider>
+      </BrowserRouter>
+    </WalletProvider>
+
+
+
+    //////////////////////////////////
+
+
+
+
+*/
