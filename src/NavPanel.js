7c9acb011fca3880f8bc0e6dc80b6536624bbf1a
abhinav anshul <abhinavanshul4u@gmail.com>
2020-07-23 16:07:32 +0530

    fixes dynamic background color, improves routing

--- src/NavPanel.js
+++ src/NavPanel.js
@@ -1,5 +1,7 @@
 /* eslint-disable */
 import React, { useState } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
 import {
   Main,
   Header,
@@ -35,17 +37,37 @@ import DashboardMain from './components/Dashboard/DashboardMain'
 import ArbitrationsPage from './components/Arbitrations/ArbitrationsPage'
 
 import '../src/reset.css'
+var primary = '#52006F'
 
-function NavPanel() {
+function NavPanel(props) {
   const [navSettings, setNavSettings] = useState('unset')
   const [navDashboard, setNavDashboard] = useState('unset')
   const [navArbitrations, setNavArbitrations] = useState('unset')
+  const [toggleBackgroundColor1, setToggleBackgroundColor1] = useState(null)
+  const [toggleColor1, setToggleColor1] = useState('black')
+  const [toggleBackgroundColor2, setToggleBackgroundColor2] = useState(null)
+  const [toggleColor2, setToggleColor2] = useState('black')
 
   function handleClickDashboard() {
     console.log('click')
     setNavDashboard('set')
     setNavSettings('unset')
     setNavArbitrations('unset')
+    setToggleBackgroundColor1(primary)
+    setToggleColor1('white')
+    setToggleBackgroundColor2(null)
+    setToggleColor2('black')
+  }
+
+  function handleClickArbitrations() {
+    console.log('click')
+    setNavArbitrations('set')
+    setNavDashboard('unset')
+    setNavSettings('unset')
+    setToggleBackgroundColor2(primary)
+    setToggleColor2('white')
+    setToggleBackgroundColor1(null)
+    setToggleColor1('black')
   }
 
   function handleClickSettings() {
@@ -55,290 +77,289 @@ function NavPanel() {
     setNavArbitrations('unset')
   }
 
-  function handleClickArbitrations() {
-    console.log('click')
-    setNavArbitrations('set')
-    setNavDashboard('unset')
-    setNavSettings('unset')
-  }
-
+  console.log(props.link)
   return (
     <>
-      <Main layout={false}>
-        <div
-          style={{
-            paddingLeft: '2rem',
-            paddingRight: '2rem',
-            borderBottom: '0.5px solid #e5e5e5',
-          }}
-        >
-          <Header
+      <div>
+        <Main layout={false}>
+          <div
             style={{
-              paddingTop: '1rem',
-              paddingBottom: '1rem',
+              paddingLeft: '2rem',
+              paddingRight: '2rem',
+              borderBottom: '0.5px solid #e5e5e5',
             }}
-            primary={
-              <div
-                style={{
-                  display: 'flex',
-                }}
-              >
-                <div>
-                  <img style={{ width: '70%' }} src={BrandLogo} />
-                </div>
+          >
+            <Header
+              style={{
+                paddingTop: '1rem',
+                paddingBottom: '1rem',
+              }}
+              primary={
                 <div
                   style={{
                     display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    fontSize: '1.5rem',
-                    letterSpacing: '0.5px',
                   }}
                 >
-                  Arbchain
+                  <div>
+                    <img style={{ width: '70%' }} src={BrandLogo} />
+                  </div>
+                  <div
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      justifyContent: 'center',
+                      fontSize: '1.5rem',
+                      letterSpacing: '0.5px',
+                    }}
+                  >
+                    Arbchain
+                  </div>
                 </div>
-              </div>
-            }
-            secondary={
-              <div style={{ display: 'flex' }}>
-                <div
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    marginLeft: '1.5rem',
-                    marginRight: '1.5rem',
-                  }}
-                >
-                  <img
-                    style={{ width: '70%' }}
-                    alt='notification'
-                    src={Notification}
-                  />
-                </div>
-
+              }
+              secondary={
                 <div style={{ display: 'flex' }}>
                   <div
                     style={{
                       display: 'flex',
                       flexDirection: 'column',
                       justifyContent: 'center',
+                      marginLeft: '1.5rem',
+                      marginRight: '1.5rem',
                     }}
                   >
                     <img
                       style={{ width: '70%' }}
                       alt='notification'
-                      src={Avatar}
+                      src={Notification}
                     />
                   </div>
-                  <div
-                    style={{
-                      display: 'flex',
-                      flexDirection: 'column',
-                      justifyContent: 'center',
-                    }}
-                  >
-                    <ContextMenu>
-                      <ContextMenuItem>Log In</ContextMenuItem>
-                      <ContextMenuItem>Log Out</ContextMenuItem>
-                    </ContextMenu>
+
+                  <div style={{ display: 'flex' }}>
+                    <div
+                      style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                      }}
+                    >
+                      <img
+                        style={{ width: '70%' }}
+                        alt='notification'
+                        src={Avatar}
+                      />
+                    </div>
+                    <div
+                      style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                      }}
+                    >
+                      <ContextMenu>
+                        <ContextMenuItem>Log In</ContextMenuItem>
+                        <ContextMenuItem>Log Out</ContextMenuItem>
+                      </ContextMenu>
+                    </div>
                   </div>
                 </div>
-              </div>
-            }
-          />
-        </div>
+              }
+            />
+          </div>
 
-        <div style={{ display: 'flex' }}>
-          <div
-            style={{
-              width: '18%',
-              textAlign: 'left',
-              height: '86.8vh',
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'space-between',
-              borderRight: '0.5px solid #e5e5e5',
-            }}
-          >
+          <div style={{ display: 'flex' }}>
             <div
               style={{
-                marginTop: '0rem',
+                width: '18%',
+                textAlign: 'left',
+                height: '86.8vh',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+                borderRight: '0.5px solid #e5e5e5',
               }}
             >
-              <ul>
-                <li>
-                  <div
-                    style={{
-                      display: 'grid',
-                      gridTemplateColumns: '1fr 3fr',
-                      backgroundColor: '#E2F3F5',
-                      cursor: 'pointer',
-                      gridColumnGap: '1rem',
-                      padding: '0.5rem 0rem',
-                    }}
-                  >
+              <div
+                style={{
+                  marginTop: '0rem',
+                }}
+              >
+                <ul>
+                  <li>
                     <div
                       style={{
-                        // justifySelf: 'end',
-                        // alignSelf: 'center',
-                        // textAlign: 'center',
-                        display: 'flex',
-                        justifyContent: 'flex-end',
+                        display: 'grid',
+                        gridTemplateColumns: '1fr 3fr',
+                        // backgroundColor: '#E2F3F5',
+                        backgroundColor: toggleBackgroundColor1,
+                        cursor: 'pointer',
+                        gridColumnGap: '1rem',
+                        padding: '0.5rem 0rem',
+                        color: toggleColor1,
                       }}
                     >
-                      <img
-                        style={{ alignSelf: 'center', justifySelf: 'end' }}
-                        src={DashboardIcon}
-                      />
+                      <div
+                        style={{
+                          // justifySelf: 'end',
+                          // alignSelf: 'center',
+                          // textAlign: 'center',
+                          display: 'flex',
+                          justifyContent: 'flex-end',
+                        }}
+                      >
+                        <img
+                          style={{ alignSelf: 'center', justifySelf: 'end' }}
+                          src={DashboardIcon}
+                        />
+                      </div>
+                      <div
+                        onClick={handleClickDashboard}
+                        style={{
+                          display: 'flex',
+                          justifyContent: 'flex-start',
+                          paddingTop: '4px',
+                        }}
+                      >
+                        DASHBOARD
+                      </div>
                     </div>
+                  </li>
+                  <li>
                     <div
-                      onClick={handleClickDashboard}
                       style={{
-                        display: 'flex',
-                        justifyContent: 'flex-start',
-                        paddingTop: '4px',
+                        display: 'grid',
+                        gridTemplateColumns: '1fr 3fr',
+                        cursor: 'pointer',
+                        backgroundColor: toggleBackgroundColor2,
+                        color: toggleColor2,
+                        gridColumnGap: '1rem',
+                        padding: '0.5rem 0rem',
                       }}
                     >
-                      DASHBOARD
+                      <div
+                        style={{
+                          // justifySelf: 'end',
+                          // alignSelf: 'center',
+                          // textAlign: 'center',
+                          display: 'flex',
+                          justifyContent: 'flex-end',
+                        }}
+                      >
+                        <img
+                          style={{ alignSelf: 'center', justifySelf: 'end' }}
+                          src={ArbitrationsIcon}
+                        />
+                      </div>
+                      <div
+                        onClick={handleClickArbitrations}
+                        style={{
+                          display: 'flex',
+                          justifyContent: 'flex-start',
+                          paddingTop: '4px',
+                        }}
+                      >
+                        ARBITRATION
+                      </div>
                     </div>
-                  </div>
-                </li>
-                <li>
-                  <div
-                    style={{
-                      display: 'grid',
-                      gridTemplateColumns: '1fr 3fr',
-                      cursor: 'pointer',
+                  </li>
+                  <li>
+                    <div
+                      style={{
+                        display: 'grid',
+                        gridTemplateColumns: '1fr 3fr',
+                        cursor: 'pointer',
 
-                      gridColumnGap: '1rem',
-                      padding: '0.5rem 0rem',
-                    }}
-                  >
-                    <div
-                      style={{
-                        // justifySelf: 'end',
-                        // alignSelf: 'center',
-                        // textAlign: 'center',
-                        display: 'flex',
-                        justifyContent: 'flex-end',
+                        gridColumnGap: '1rem',
+                        padding: '0.5rem 0rem',
                       }}
                     >
-                      <img
-                        style={{ alignSelf: 'center', justifySelf: 'end' }}
-                        src={ArbitrationsIcon}
-                      />
+                      <div
+                        style={{
+                          // justifySelf: 'end',
+                          // alignSelf: 'center',
+                          // textAlign: 'center',
+                          display: 'flex',
+                          justifyContent: 'flex-end',
+                        }}
+                      >
+                        <img
+                          style={{ alignSelf: 'center', justifySelf: 'end' }}
+                          src={ProfileIcon}
+                        />
+                      </div>
+                      <div
+                        style={{
+                          display: 'flex',
+                          justifyContent: 'flex-start',
+                          paddingTop: '4px',
+                        }}
+                      >
+                        PROFILE
+                      </div>
                     </div>
-                    <div
-                      onClick={handleClickArbitrations}
-                      style={{
-                        display: 'flex',
-                        justifyContent: 'flex-start',
-                        paddingTop: '4px',
-                      }}
-                    >
-                      ARBITRATION
-                    </div>
-                  </div>
-                </li>
-                <li>
-                  <div
-                    style={{
-                      display: 'grid',
-                      gridTemplateColumns: '1fr 3fr',
-                      cursor: 'pointer',
+                  </li>
+                </ul>
+              </div>
 
-                      gridColumnGap: '1rem',
-                      padding: '0.5rem 0rem',
-                    }}
-                  >
+              <div>
+                <ul>
+                  <li>
                     <div
+                      onClick={handleClickSettings}
                       style={{
-                        // justifySelf: 'end',
-                        // alignSelf: 'center',
-                        // textAlign: 'center',
-                        display: 'flex',
-                        justifyContent: 'flex-end',
+                        display: 'grid',
+                        gridTemplateColumns: '1fr 3fr',
+                        cursor: 'pointer',
+
+                        gridColumnGap: '1rem',
+                        padding: '0.5rem 0rem',
                       }}
                     >
-                      <img
-                        style={{ alignSelf: 'center', justifySelf: 'end' }}
-                        src={ProfileIcon}
-                      />
+                      <div
+                        style={{
+                          // justifySelf: 'end',
+                          // alignSelf: 'center',
+                          // textAlign: 'center',
+
+                          display: 'flex',
+                          justifyContent: 'flex-end',
+                          cursor: 'pointer',
+                        }}
+                      >
+                        <img
+                          style={{ alignSelf: 'center', justifySelf: 'end' }}
+                          src={SettingsIcon}
+                        />
+                      </div>
+
+                      <div
+                        style={{
+                          display: 'flex',
+                          justifyContent: 'flex-start',
+                          cursor: 'pointer',
+                          paddingTop: '4px',
+                        }}
+                      >
+                        SETTINGS
+                      </div>
                     </div>
-                    <div
-                      style={{
-                        display: 'flex',
-                        justifyContent: 'flex-start',
-                        paddingTop: '4px',
-                      }}
-                    >
-                      PROFILE
-                    </div>
-                  </div>
-                </li>
-              </ul>
+                  </li>
+                </ul>
+              </div>
             </div>
-
-            <div>
-              <ul>
-                <li>
-                  <div
-                    onClick={handleClickSettings}
-                    style={{
-                      display: 'grid',
-                      gridTemplateColumns: '1fr 3fr',
-                      cursor: 'pointer',
-
-                      gridColumnGap: '1rem',
-                      padding: '0.5rem 0rem',
-                    }}
-                  >
-                    <div
-                      style={{
-                        // justifySelf: 'end',
-                        // alignSelf: 'center',
-                        // textAlign: 'center',
-
-                        display: 'flex',
-                        justifyContent: 'flex-end',
-                        cursor: 'pointer',
-                      }}
-                    >
-                      <img
-                        style={{ alignSelf: 'center', justifySelf: 'end' }}
-                        src={SettingsIcon}
-                      />
-                    </div>
-
-                    <div
-                      style={{
-                        display: 'flex',
-                        justifyContent: 'flex-start',
-                        cursor: 'pointer',
-                        paddingTop: '4px',
-                      }}
-                    >
-                      SETTINGS
-                    </div>
-                  </div>
-                </li>
-              </ul>
+            <div style={{ backgroundColor: '#FBFCFD', width: '80%' }}>
+              {/* //// */}
+              {navSettings === 'set' ? (
+                <h1>This is a settings page</h1>
+              ) : (
+                <p>{null}</p>
+              )}
+              {navDashboard === 'set' ? <DashboardMain /> : <p>{null}</p>}
+              {navArbitrations === 'set' ? <ArbitrationsPage /> : <p>{null}</p>}
+              {/* //// */}
             </div>
           </div>
-          <div style={{ backgroundColor: '#FBFCFD', width: '80%' }}>
-            {/* //// */}
-            {navSettings === 'set' ? (
-              <h1>This is a settings page</h1>
-            ) : (
-              <p>{null}</p>
-            )}
-            {navDashboard === 'set' ? <DashboardMain /> : <p>{null}</p>}
-            {navArbitrations === 'set' ? <ArbitrationsPage /> : <p>{null}</p>}
-            {/* //// */}
-          </div>
-        </div>
-      </Main>
+        </Main>
+      </div>
     </>
   )
 }
