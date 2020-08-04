0ee8176d6c7e2cc8a75bc27e9b33261848ff2e1c
abhinav anshul <abhinavanshul4u@gmail.com>
2020-08-04 11:35:19 +0530

    adds dashboard route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-/* eslint-disable prettier/prettier */
+/* eslint-disable  */
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Dashboard from './components/Dashboard/Dashboard'
 
 import NavPanelREDUNDANT from './NavPanelREDUNDANT'
 
@@ -8,16 +9,17 @@ function App() {
   return (
     <>
    {/* <NavPanelREDUNDANT /> */}
-    <BrowserRouter>
-    {/* <Switch>
-              <Route exact path='/profile' component={Dashboard} />
-              <Route exact path='/leaderboard' component={Tasks} />
+   {/* <Dashboard /> */}
+     <BrowserRouter>
+    <Switch>
+              <Route exact path='/dashboard' component={Dashboard} />
+              {/* <Route exact path='/leaderboard' component={Tasks} />
               <Route exact path='/dashboard' component={Disputes} />
               <Route exact path='/sample' component={Sample} />
 
-              <Route component={ErrorPage} />
-            </Switch> */}
-    </BrowserRouter>
+              <Route component={ErrorPage} /> */}
+            </Switch> 
+    </BrowserRouter> 
 
    </>
   )
