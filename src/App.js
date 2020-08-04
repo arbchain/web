2a2c482c4dc740c3df7d409a1b837608dc070f98
abhinav anshul <abhinavanshul4u@gmail.com>
2020-08-04 11:54:58 +0530

    adds arbitration route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Dashboard from './components/Dashboard/Dashboard'
 
 import NavPanelREDUNDANT from './NavPanelREDUNDANT'
+import Arbitrations from './components/Arbitrations/Arbitrations'
 
 function App() {
   return (
@@ -13,11 +14,11 @@ function App() {
      <BrowserRouter>
     <Switch>
               <Route exact path='/dashboard' component={Dashboard} />
-              {/* <Route exact path='/leaderboard' component={Tasks} />
-              <Route exact path='/dashboard' component={Disputes} />
+              <Route exact path='/arbitration' component={Arbitrations} />
+              {/* <Route exact path='/dashboard' component={Disputes} />
               <Route exact path='/sample' component={Sample} />
 
-              <Route component={ErrorPage} /> */}
+              <Route component={ErrorPage} /> */} 
             </Switch> 
     </BrowserRouter> 
 
--- src/components/Arbitrations/Arbitrations.js
+++ src/components/Arbitrations/Arbitrations.js
@@ -1,151 +1,25 @@
 /* eslint-disable */
-import React from 'react'
+import React from 'react';
+import { Main } from '@aragon/ui'
+import HeaderPanel from '../../HeaderPanel'
+import NavPanelNew from '../../NavPanelNew'
 import ArbitrationsPage from '../Arbitrations/ArbitrationsPage'
-import {
-  Main,
-  Header,
-  Button,
-  IconPlus,
-  Tag,
-  SidePanel,
-  Split,
-  DataView,
-  Box,
-  GU,
-  Layout,
-  textStyle,
-  DropDown,
-  IdentityBadge,
-} from '@aragon/ui'
-import { IconNotifications } from '@aragon/ui'
-import { FaBeer } from 'react-icons/fa'
-import { IoIosNotifications } from 'react-icons/io'
-import BrandLogo from '../../assets/arbchainlogo.svg'
-import Notification from '../../assets/notification.svg'
-import Avatar from '../../assets/avatar.svg'
-import '../../reset.css'
-import { Link } from 'react-router-dom'
 
-function Arbitrations() {
-  return (
+function Arbitrations(){
+  return(
     <>
-      <Main layout={false}>
-        <div style={{ marginLeft: '5rem', marginRight: '5rem' }}>
-          <Header
-            primary={
-              <div
-                style={{
-                  display: 'flex',
-                }}
-              >
-                <div>
-                  <img style={{ width: '70%' }} src={BrandLogo} />
-                </div>
-                <div
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    fontSize: '1.5rem',
-                    letterSpacing: '1px',
-                  }}
-                >
-                  Arbchain
-                </div>
-              </div>
-            }
-            secondary={
-              <div style={{ display: 'flex' }}>
-                <div
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    marginLeft: '1.5rem',
-                    marginRight: '1.5rem',
-                  }}
-                >
-                  <img
-                    style={{ width: '70%' }}
-                    alt='notification'
-                    src={Notification}
-                  />
-                </div>
-
-                <div style={{ display: 'flex' }}>
-                  <div
-                    style={{
-                      display: 'flex',
-                      flexDirection: 'column',
-                      justifyContent: 'center',
-                    }}
-                  >
-                    <img
-                      style={{ width: '70%' }}
-                      alt='notification'
-                      src={Avatar}
-                    />
-                  </div>
-                  <div
-                    style={{
-                      display: 'flex',
-                      flexDirection: 'column',
-                      justifyContent: 'center',
-                    }}
-                  >
-                    <DropDown
-                      placeholder='Consenso Corp'
-                      items={['LogIn', 'LogOut']}
-                    />
-                  </div>
-                </div>
-              </div>
-            }
-          />
-        </div>
-
-        <div style={{ display: 'flex' }}>
-          <div
-            style={{
-              // backgroundColor: 'pink',
-              width: '20%',
-              textAlign: 'left',
-              height: '86.8vh',
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'space-between',
-            }}
-          >
-            <div style={{ marginTop: '0rem' }}>
-              <ul>
-                <li>
-                  <Button wide>DASHBOARD</Button>
-                </li>
-                <li>
-                  <Button wide>
-                    {' '}
-                    <Link to='/arbitrations'> ARBITRATIONS</Link>
-                  </Button>
-                </li>
-                <li>
-                  <Button wide>PROFILE</Button>
-                </li>
-              </ul>
-            </div>
-
-            <div>
-              <ul>
-                <li>
-                  <Button wide>Settings</Button>
-                </li>
-              </ul>
+       <Main layout={false}>
+      <HeaderPanel />
+      <NavPanelNew />
+      
+          <div style={{ display: 'flex' }}>
+            
+            <div style={{ backgroundColor: '#FBFCFD', width: '100%' }}>
+                <ArbitrationsPage />
             </div>
           </div>
-          <div style={{ backgroundColor: '#FBFCFD', width: '80%' }}>
-            <ArbitrationsPage />
-          </div>
-        </div>
-      </Main>
+        </Main>
+      
     </>
   )
 }
