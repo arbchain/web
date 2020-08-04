2de79e0367643bc75258b1032cdf79ecbfe94736
abhinav anshul <abhinavanshul4u@gmail.com>
2020-08-04 11:58:14 +0530

    refactors app file

--- src/App.js
+++ src/App.js
@@ -2,24 +2,18 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Dashboard from './components/Dashboard/Dashboard'
-
-import NavPanelREDUNDANT from './NavPanelREDUNDANT'
 import Arbitrations from './components/Arbitrations/Arbitrations'
+import ErrorPage from './ErrorPage'
 
 function App() {
   return (
     <>
-   {/* <NavPanelREDUNDANT /> */}
-   {/* <Dashboard /> */}
-     <BrowserRouter>
-    <Switch>
-              <Route exact path='/dashboard' component={Dashboard} />
-              <Route exact path='/arbitration' component={Arbitrations} />
-              {/* <Route exact path='/dashboard' component={Disputes} />
-              <Route exact path='/sample' component={Sample} />
-
-              <Route component={ErrorPage} /> */} 
-            </Switch> 
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/dashboard' component={Dashboard} />
+        <Route exact path='/arbitration' component={Arbitrations} />
+        <Route component={ErrorPage} />  
+      </Switch> 
     </BrowserRouter> 
 
    </>
