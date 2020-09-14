0ed516885948b476ea01a295e9ee525c2f8c0f38
koushith <koushith97@gmail.com>
2020-09-14 14:07:30 +0530

    adds stepper and signUp form

--- src/components/Login/SignUp.js
+++ src/components/Login/SignUp.js
@@ -1,35 +1,36 @@
 import React, { useState } from 'react';
-import {
-  Layout,
-  Steps,
-  Row,
-  Col,
-  Select,
-  Form,
-  Button,
-  Input,
-  InputNumber,
-  Divider,
-} from 'antd';
+import { Steps, Select, Form, Button, Input } from 'antd';
 import wallet from 'wallet-besu';
 import { Main } from '@aragon/ui';
 import 'antd/dist/antd.css';
 import './SingnUp.Style.css';
 
 // Destructing
-const { Header, Content, Footer, Sider } = Layout;
-const { Password } = Input;
+//const { Header, Content, Footer, Sider } = Layout;
 const { Step } = Steps;
 const { Option, OptGroup } = Select;
 
+// Form Layout
 const layout = {
   labelCol: {
-    span: 8,
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 8,
+    },
   },
   wrapperCol: {
-    span: 16,
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
   },
 };
+
+// Form Validation
 const validateMessages = {
   required: '${label} is required!',
   types: {
@@ -41,18 +42,6 @@ const validateMessages = {
   },
 };
 
-// const prefixSelector = (
-//   <Form.Item name='prefix' noStyle>
-//     <Select
-//       style={{
-//         width: 70,
-//       }}
-//     >
-//       <Option value='86'>+86</Option>
-//       <Option value='87'>+87</Option>
-//     </Select>
-//   </Form.Item>
-// );
 const Signup = () => {
   // state for stepper
   const [current, setCurrent] = useState(0);
@@ -78,138 +67,241 @@ const Signup = () => {
   // password
   const [password, setPassword] = useState('');
 
+  // Stepper Handler
+  function next() {
+    setCurrent(current + 1);
+  }
+
+  function prev() {
+    setCurrent(current - 1);
+  }
+
   function handleChange(value) {
     console.log(`selected ${value}`);
   }
 
-  async function registerUser() {
-    const status = await wallet.create(password, 'oriankey123');
-    console.log(status);
+  // Form Handlers
+
+  // Dropdowns
+  function selectRole(value) {
+    console.log(`Selected role : ${value}`);
+    setRole(value);
   }
 
-  const submitForm = () => {
-    const onFinish = (values) => {
-      console.log(values);
-    };
-  };
+  function selectNetwork(value) {
+    console.log(`Selected role : ${value}`);
+    setNetwork(value);
+  }
+
+  // Test
+
+  console.log(name);
+  console.log(zip);
+  console.log(phone);
+  console.log(email);
+  console.log(role);
+  console.log(password);
+
+  let networkSelection = (
+    <Form
+      {...layout}
+      id='form'
+      name='nest-messages'
+      // onFinish={registerUser}
+      validateMessages={validateMessages}
+    >
+      <Form.Item label='Select Network'>
+        <Select label='Field A' defaultValue='Node1' onChange={selectNetwork}>
+          <OptGroup label='Networks'>
+            <Option value={network}>Node1</Option>
+            <Option value='n2'>Node2</Option>
+          </OptGroup>
+        </Select>
+        <Button
+          type='primary'
+          style={{ width: '100%', marginTop: '16px', background: '#4d4cbb' }}
+        >
+          Generate New Account
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+
+  let userInformation = (
+    <>
+      <Form
+        {...layout}
+        id='form'
+        name='nest-messages'
+        // onFinish={registerUser}
+        validateMessages={validateMessages}
+      >
+        <Form.Item
+          name={['user', 'name']}
+          label='Name'
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input value={name} onChange={(e) => setName(e.target.value)} />
+        </Form.Item>
+
+        <Form.Item
+          name='zip'
+          label='Zip Code'
+          rules={[
+            {
+              required: true,
+              message: 'Please enter your Zip code!',
+            },
+          ]}
+        >
+          <Input
+            style={{ width: '100%' }}
+            value={zip}
+            onChange={(e) => setZip(e.target.value)}
+          />
+        </Form.Item>
+
+        <Form.Item
+          name='phone'
+          label='Phone Number'
+          rules={[
+            {
+              required: true,
+              message: 'Please input your phone number!',
+            },
+          ]}
+        >
+          <Input value={phone} onChange={(e) => setPhone(e.target.value)} />
+        </Form.Item>
+        <Form.Item
+          name={['user', 'email']}
+          label='Email'
+          rules={[
+            {
+              type: 'email',
+            },
+          ]}
+        >
+          <Input value={email} onChange={(e) => setEmail(e.target.value)} />
+        </Form.Item>
+
+        <Form.Item label='Role'>
+          <Select label='Role' defaultValue='Role' onChange={selectRole}>
+            <OptGroup label='Networks'>
+              <Option value={role}>Arbitrator</Option>
+              <Option value='Role2'>Claiment</Option>
+            </OptGroup>
+          </Select>
+        </Form.Item>
+        <Form.Item label='Password'>
+          <Input.Password
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Item>
+
+        {/* <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+          <Button type='primary' htmlType='submit'>
+            Sign up
+          </Button>
+        </Form.Item> */}
+      </Form>
+    </>
+  );
+
+  // Preview:- Readonly
+
+  let preview = (
+    <>
+      <Form
+        {...layout}
+        id='form'
+        name='nest-messages'
+        // onFinish={registerUser}
+        validateMessages={validateMessages}
+      >
+        <Form.Item name={['user', 'name']} label='Name'>
+          <Input value={name} />
+        </Form.Item>
+
+        <Form.Item name='zip' label='Zip Code'>
+          <Input style={{ width: '100%' }} value={zip} />
+        </Form.Item>
+
+        <Form.Item name='phone' label='Phone Number'>
+          <Input value={phone} />
+        </Form.Item>
+        <Form.Item name={['user', 'email']} label='Email'>
+          <Input value={email} />
+        </Form.Item>
+
+        <Form.Item label='Role'>
+          <Select label='Role' defaultValue='Role'>
+            <OptGroup label='Networks'>
+              <Option value={role}>Arbitrator</Option>
+            </OptGroup>
+          </Select>
+        </Form.Item>
+
+        {/* <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+      <Button type='primary' htmlType='submit'>
+        Sign up
+      </Button>
+    </Form.Item> */}
+      </Form>
+    </>
+  );
+
+  // stepper
+  const steps = [
+    {
+      title: 'Select Network',
+      content: networkSelection,
+    },
+    {
+      title: 'User Information',
+      content: userInformation,
+    },
+    {
+      title: 'Create Account',
+      content: preview,
+    },
+  ];
 
   return (
     <Main layout={false}>
-      <Layout className='layout'>
-        <Content
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh',
-          }}
-        >
-          <Col span={18}>
-            <Content>
-              <div className='header'>
-                <h2>Welcome to Arbchain</h2>
-              </div>
-              <Form
-                {...layout}
-                id='form'
-                className='login-form'
-                name='nest-messages'
-                onFinish={registerUser}
-                validateMessages={validateMessages}
-              >
-                <Divider dashed />
-
-                <Form.Item label='Select Network' className='login-form'>
-                  <Select
-                    label='Field A'
-                    defaultValue='Node1'
-                    onChange={handleChange}
-                  >
-                    <OptGroup label='Networks'>
-                      <Option value='n1'>Node1</Option>
-                      <Option value='n2'>Node2</Option>
-                    </OptGroup>
-                  </Select>
-                </Form.Item>
-
-                <Form.Item
-                  name={['user', 'name']}
-                  label='Name'
-                  rules={[
-                    {
-                      required: true,
-                    },
-                  ]}
-                >
-                  <Input />
-                </Form.Item>
-
-                <Form.Item
-                  name='zip'
-                  label='Zip Code'
-                  rules={[
-                    {
-                      required: true,
-                      message: 'Please enter your Zip code!',
-                    },
-                  ]}
-                >
-                  <Input style={{ width: '100%' }} />
-                </Form.Item>
-
-                <Form.Item
-                  name='phone'
-                  label='Phone Number'
-                  rules={[
-                    {
-                      required: true,
-                      message: 'Please input your phone number!',
-                    },
-                  ]}
-                >
-                  <Input style={{ width: '100%' }} />
-                </Form.Item>
-                <Form.Item
-                  name={['user', 'email']}
-                  label='Email'
-                  rules={[
-                    {
-                      type: 'email',
-                    },
-                  ]}
-                >
-                  <Input />
-                </Form.Item>
-
-                <Form.Item label='Role'>
-                  <Select
-                    label='Role'
-                    defaultValue='Role'
-                    onChange={handleChange}
-                  >
-                    <OptGroup label='Networks'>
-                      <Option value='Role1'>Arbitrator</Option>
-                      <Option value='Role2'>Claiment</Option>
-                    </OptGroup>
-                  </Select>
-                </Form.Item>
-                <Form.Item label='Password'>
-                  <Input.Password
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </Form.Item>
-
-                <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-                  <Button type='primary' htmlType='submit'>
-                    Sign up
-                  </Button>
-                </Form.Item>
-              </Form>
-            </Content>
-          </Col>
-        </Content>
-      </Layout>
+      <div className='container'>
+        <Steps current={current} className='stepper'>
+          {steps.map((item) => (
+            <Step key={item.title} title={item.title} />
+          ))}
+        </Steps>
+        <div className='steps-content'>{steps[current].content}</div>
+        <div className='steps-action'>
+          {current < steps.length - 1 && (
+            <Button type='primary' onClick={next}>
+              Next
+            </Button>
+          )}
+          {current === steps.length - 1 && (
+            <Button
+              type='primary'
+              // onClick={() => message.success('Processing complete!')}
+            >
+              Sign up
+            </Button>
+          )}
+          {current > 0 && (
+            <Button style={{ margin: '0 8px' }} onClick={() => prev()}>
+              Previous
+            </Button>
+          )}
+        </div>
+      </div>
     </Main>
   );
 };
