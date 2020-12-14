6888fdcf250db9335d6b2e257ccd4358803109fe
yathishram <123yathish.r@gmail.com>
2020-12-14 15:15:06 +0530

    fixes timeline data fetch

--- src/components/Arbitrations/DisputeTimeline.js
+++ src/components/Arbitrations/DisputeTimeline.js
@@ -8,10 +8,12 @@ import {
   IconFlag,
   IconGroup,
   IconFundraising,
+  EmptyStateCard,
 } from '@aragon/ui';
 
 import Stepper from '../Stepper';
 import Step from '../Step';
+import { Skeleton } from 'antd';
 
 import { getTimeLine } from '../../lib/contracts/SPC';
 
@@ -19,7 +21,12 @@ function DisputeTimeline({ NODE, account, contractAddress, groupId }) {
   const theme = useTheme();
   const [loading, setLoading] = useState(true);
   const [timeline, setTimeLine] = useState(null);
-  const current = 4;
+  const [stages, setStages] = useState(null);
+
+  const getDate = value => {
+    const date = new Date(parseInt(value) * 1000);
+    return date.toDateString();
+  };
 
   useEffect(() => {
     async function getDetails() {
@@ -32,6 +39,43 @@ function DisputeTimeline({ NODE, account, contractAddress, groupId }) {
           if (res) {
             console.log(res);
             setTimeLine(res);
+            setStages([
+              {
+                label: 'Aribtration Created',
+                date: res[1],
+                Icon: IconFundraising,
+              },
+              {
+                label: 'Response Submitted',
+                date: res[2],
+                Icon: IconChat,
+              },
+              {
+                label: 'Tribunal Formed',
+                date: res[3],
+                Icon: IconChat,
+              },
+              {
+                label: 'Challenge Arbitrator',
+                date: res[4],
+                Icon: IconFolder,
+              },
+              {
+                label: 'Arbitration Started',
+                date: res[5],
+                Icon: IconFlag,
+              },
+              {
+                label: 'Nomination',
+                date: res[6],
+                Icon: IconGroup,
+              },
+              {
+                label: 'Award',
+                date: '',
+                Icon: IconFlag,
+              },
+            ]);
           }
           setLoading(false);
         }
@@ -39,47 +83,11 @@ function DisputeTimeline({ NODE, account, contractAddress, groupId }) {
         return false;
       }
     }
-    getDetails();
+    setTimeout(() => {
+      getDetails();
+    }, 3000);
   }, [account]);
   // const arbitrationCreation = new Date(contractTime * 1000);
-
-  const stages = [
-    {
-      label: 'Aribtration Created',
-      date: '20/11/2019',
-      Icon: IconFundraising,
-    },
-    {
-      label: 'Response Submitted',
-      date: '20/11/2019',
-      Icon: IconChat,
-    },
-    {
-      label: 'Tribunal Formed',
-      date: '20/11/2019',
-      Icon: IconChat,
-    },
-    {
-      label: 'Challenge Arbitrator',
-      date: '20/11/2019',
-      Icon: IconFolder,
-    },
-    {
-      label: 'Nomination',
-      date: '20/11/2019',
-      Icon: IconGroup,
-    },
-    {
-      label: 'Arbitration Started',
-      date: '20/11/2019',
-      Icon: IconFlag,
-    },
-    {
-      label: 'Award',
-      date: '20/11/2019',
-      Icon: IconFlag,
-    },
-  ];
 
   return (
     <div>
@@ -90,61 +98,71 @@ function DisputeTimeline({ NODE, account, contractAddress, groupId }) {
           padding: ${3 * GU}px 0;
         `}
       >
-        {stages.map(({ label, date, Icon }, index) => {
-          const active = current === index;
-          return (
-            <Step
-              key={index}
-              active={active}
-              stepPoint={
-                <div
-                  css={`
-                    background: ${active ? theme.selected : '#ECEFF4'};
-                    border-radius: 80%;
-                    padding: 10px;
-                    position: relative;
-                    z-index: 2;
-                    display: inline-flex;
-                  `}
-                >
-                  <Icon color={active ? '#fff' : theme.surfaceIcon} />
-                </div>
-              }
-              content={
-                <div>
-                  <div>
-                    <span css={textStyle('body1')}>{label}</span>
+        {loading ? (
+          <>
+            <Skeleton active />
+            <Skeleton active />
+            <Skeleton active />
+          </>
+        ) : timeline ? (
+          stages.map(({ label, date, Icon }, index) => {
+            const active = parseInt(timeline[0]) === index;
+            return (
+              <Step
+                key={index}
+                active={active}
+                stepPoint={
+                  <div
+                    css={`
+                      background: ${active ? theme.selected : '#ECEFF4'};
+                      border-radius: 80%;
+                      padding: 10px;
+                      position: relative;
+                      z-index: 2;
+                      display: inline-flex;
+                    `}
+                  >
+                    <Icon color={active ? '#fff' : theme.surfaceIcon} />
                   </div>
+                }
+                content={
                   <div>
-                    <span
-                      css={`
-                        color: ${theme.contentSecondary};
-                        opacity: 0.6;
-                      `}
-                    >
-                      {date}
-                    </span>
-                  </div>
-                  {active && (
+                    <div>
+                      <span css={textStyle('body1')}>{label}</span>
+                    </div>
                     <div>
                       <span
                         css={`
-                          ${textStyle('label3')}
-                          text-transform: Uppercase;
-                          background: rgba(200, 215, 234, 0.4);
-                          border-radius: 100px;
-                          padding: 5px 10px;
+                          color: ${theme.contentSecondary};
+                          opacity: 0.6;
                         `}
                       >
-                        current
+                        {parseInt(date) > 0 ? getDate(date) : ''}
                       </span>
                     </div>
-                  )}
-                </div>
-              }
-            />
-          );
-        })}
+                    {active && (
+                      <div>
+                        <span
+                          css={`
+                            ${textStyle('label3')}
+                            text-transform: Uppercase;
+                            background: rgba(200, 215, 234, 0.4);
+                            border-radius: 100px;
+                            padding: 5px 10px;
+                          `}
+                        >
+                          current
+                        </span>
+                      </div>
+                    )}
+                  </div>
+                }
+              />
+            );
+          })
+        ) : (
+          <EmptyStateCard text="No Timeline details found." />
+        )}
       </Stepper>
     </div>
   );
