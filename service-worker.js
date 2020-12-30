c19174adc96442378921df134fd6ec1c8f904a6b
koshikraj <koshik.raj@gmail.com>
2020-12-30 13:14:18 +0000

    Deploying to gh-pages from  @ f74222edc32b90d9d5db4cc55e333f5222d1c72f 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7af72ecccd54a339f39c87f8ff528807.js"
+  "/precache-manifest.bbaa81df005a817d36307c2ea0a15cb3.js"
 );
 
 self.addEventListener('message', (event) => {
